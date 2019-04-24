1711fb76f4c19a8643876aa7308aa25a7e179b02
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-04-24 18:29:18 +0800

    commit

--- src/main/java/top/moxingwang/websocket/jmeter/WebsocketTest.java
+++ src/main/java/top/moxingwang/websocket/jmeter/WebsocketTest.java
@@ -1,40 +1,56 @@
-/*
 package top.moxingwang.websocket.jmeter;
+
 
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
 import org.springframework.messaging.converter.StringMessageConverter;
-import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
-*/
-/**
- * @description:
- * @author: MoXingwang 2019-04-23 18:56
- **//*
+import java.util.concurrent.CountDownLatch;
+
 
 public class WebsocketTest extends AbstractJavaSamplerClient {
+    public static void mainTest() {
+
+        {
+            new Thread(() -> {
+                WebSocketClient transport = new StandardWebSocketClient();
+                WebSocketStompClient stompClient = new WebSocketStompClient(transport);
+                stompClient.setMessageConverter(new StringMessageConverter());
+                String url = "wss://ordersocket.mklmall.com/p-trade-oc-bweb/websocket/trade/order/paid";
+                ListenableFuture<StompSession> future = stompClient.connect(url, new MyStompSessionHandler());
+            }).start();
+
+        }
+
+
+    }
+
     @Override
     public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
         SampleResult result = new SampleResult();
         result.sampleStart();
         try {
-            WebSocketClient webSocketClient = new StandardWebSocketClient();
-            WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
-            stompClient.setMessageConverter(new StringMessageConverter());
 
-            String url = "ws://localhost:8080/ws/test1";
-            StompSessionHandler sessionHandler = new MyStompSessionHandler();
-            stompClient.connect(url, sessionHandler);
+            WebsocketTest.mainTest();
 
 
         } finally {
             result.sampleEnd();
         }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return result;
     }
 }
-*/
--- src/test/java/top/moxingwang/websocket/SocketClientTest.java
+++ src/test/java/top/moxingwang/websocket/SocketClientTest.java
@@ -1,12 +1,6 @@
 package top.moxingwang.websocket;
 
-import org.springframework.messaging.converter.StringMessageConverter;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.web.socket.client.WebSocketClient;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-import top.moxingwang.websocket.jmeter.MyStompSessionHandler;
+import top.moxingwang.websocket.jmeter.WebsocketTest;
 
 import java.util.concurrent.CountDownLatch;
 
@@ -18,16 +12,7 @@ public class SocketClientTest {
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(1);
 
-        {
-            new Thread(() -> {
-                WebSocketClient transport = new StandardWebSocketClient();
-                WebSocketStompClient stompClient = new WebSocketStompClient(transport);
-                stompClient.setMessageConverter(new StringMessageConverter());
-                String url = "ws://localhost:53105/p-trade-oc-bweb/websocket/trade/order/paid";
-                ListenableFuture<StompSession> future = stompClient.connect(url, new MyStompSessionHandler());
-            }).start();
-
-        }
+        WebsocketTest.mainTest();
 
         countDownLatch.await();
 
